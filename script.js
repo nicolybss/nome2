2b3807a71e9b37571e0759f1ac8643da2ab3279c
NicolyBaesso <nicolybaesso18@gmail.com>
2025-04-10 10:25:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var nomes = ["Helena", "Cecília", "Maitê", "Laura", "Alice", "Isabella", "Sophia", "Aurora", "Ayla", "Heloísa", "Eloá", "Manuela", "Liz", "Antonela", "Maria", "Cecília", "Ísis", "Elisa", "Maria", "Alice", "Maysa", "Esther", "Melissa", "Maria Luiza", "Lívia", "Isadora", "Olívia", "Nicoly", "Kali", "Kailany"]
+var nomeEscolhido = parseInt(prompt("Digite o número de 0 a 29"))
+document.write("Nome Escolhido:" + nomes[nomeEscolhido])
+
